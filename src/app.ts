3eab4967292533f28f130b15dcc22dafe6a17f2c
jonli <jonli@tencent.com>
2017-10-16 09:27:36 +0800

    test

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import {  App } from './utils';
+import { App } from './utils';
 
 function main() {
-    const app = new App();
-    app.startOnInit('0.0.1');
+  const app = new App();
+  app.startOnInit('0.0.1');
 }
 
 main();
